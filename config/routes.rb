302787909c32b10ce370e21e43f97deaf63b8779
cassidyhardin <cassidyhardin@berkeley.edu>
2021-02-01 23:23:19 -0800

    controller structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :homes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'home#main', :as => 'home'
-
+  # get '/' => 'home#main', :as => 'home'
+  root 'homes#index'
 end
